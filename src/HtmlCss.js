6975f10bdcb0171474b4c6a50dad3bd9497e2470
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-05-21 14:50:34 +0100

    html working

--- src/HtmlCss.js
+++ src/HtmlCss.js
@@ -1,27 +1,34 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-
-const HtmlCss = ()=>{
-    const [updateData, setUpdateData] = useState({});
-let dataToUse;
-useEffect(()=>{
+const HtmlCss = () => {
+  const [updateData, setUpdateData] = useState([]);
+  let [Counter, setCounter] = useState(0);
+  useEffect(() => {
     const url = "https://ldn8-journey-server.glitch.me/";
     console.log("hello");
     fetch(url)
-    .then(res => res.json())
-    .then(data => setUpdateData(data))
-},[updateData])
+      .then((res) => res.json())
+      .then((data) => setUpdateData(data));
+  }, []);
 
-function handleData() {
-        
-        
-    }
-    return(
+  console.log("7");
+  console.log(updateData);
+
+  function handleData() {
+    setCounter += 1;
+    return <div>"DATA"</div>;
+  }
+
+  return (
     <div className="htmlcss-container">
-            <button onClick={handleData} >HTML/CSS</button>
-            <p> {updateData[0]}</p>
-        </div>
-    )
+      <button onClick={handleData}>HTML/CSS</button>
+      {updateData.map((d) => (
+        <div>{d.message}</div>
 
-}
+        // <div>{JSON.stringify(d)}</div>
+      ))}
+    </div>
+  );
+};
+
 export default HtmlCss;
